4d8c449efcbd55a28300c591694d8a34eb4c4e77
Anthony Macias <anthoxbv@gmail.com>
2026-06-22 09:42:38 -0500

    feat: actualizar estructuras de modelos para incluir claves primarias en Usuario, Pescador, Embarcacion, Especie, Captura, Bodega y Stock

--- go.mod
+++ go.mod
@@ -3,3 +3,12 @@ module Proyecto_Aplicaciones_Web_II
 go 1.26.2
 
 require github.com/go-chi/chi/v5 v5.3.0
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+	golang.org/x/text v0.20.0 // indirect
+	gorm.io/driver/sqlite v1.6.0 // indirect
+	gorm.io/gorm v1.31.1 // indirect
+)
--- internal/models/pesca.go
+++ internal/models/pesca.go
@@ -1,7 +1,7 @@
 package models
 
 type Usuario struct {
-	Id           int    `json:"id"`
+	Id           int    `gorm:"primary_key"`
 	Nombre       string `json:"nombre"`
 	Email        string `json:"email"`
 	PasswordHash string `json:"password_hash"`
@@ -11,7 +11,7 @@ type Usuario struct {
 }
 
 type Pescador struct {
-	ID              int    `json:"id"`
+	ID              int    `gorm:"primary_key"`
 	UsuarioID       int    `json:"usuario_id"`
 	Cedula          string `json:"cedula"`
 	ExperienciaAños int    `json:"experiencia_años"`
@@ -20,7 +20,7 @@ type Pescador struct {
 }
 
 type Embarcacion struct {
-	ID          int     `json:"id"`
+	ID          int     `gorm:"primary_key"`
 	PescadorID  int     `json:"pescador_id"`
 	Nombre      string  `json:"nombre"`
 	Matricula   string  `json:"matricula"`
@@ -28,7 +28,7 @@ type Embarcacion struct {
 	Estado      bool    `json:"estado"`
 }
 type Especie struct {
-	ID               int    `json:"id"`
+	ID               int    `gorm:"primary_key"`
 	NombreComun      string `json:"nombre_comun"`
 	NombreCientifico string `json:"nombre_cientifico"`
 	UnidadMedida     string `json:"unidad_medida"`
@@ -37,7 +37,7 @@ type Especie struct {
 }
 
 type Captura struct {
-	ID             int     `json:"id"`
+	ID             int     `gorm:"primary_key"`
 	EmbarcacionID  int     `json:"embarcacion_id"`
 	EspecieID      int     `json:"especie_id"`
 	Fecha          string  `json:"fecha"`
@@ -47,7 +47,7 @@ type Captura struct {
 }
 
 type Bodega struct {
-	ID          int     `json:"id"`
+	ID          int     `gorm:"primary_key"`
 	Nombre      string  `json:"nombre"`
 	Ubicacion   string  `json:"ubicacion"`
 	CapacidadKG float64 `json:"capacidad_kg"`
@@ -55,7 +55,7 @@ type Bodega struct {
 }
 
 type Stock struct {
-	ID           int     `json:"id"`
+	ID           int     `gorm:"primary_key"`
 	BodegaID     int     `json:"bodega_id"`
 	EspecieID    int     `json:"especie_id"`
 	CantidadKG   float64 `json:"cantidad_kg"`
